442fa84292f1e18bc26645e70e6227f741b3bf6d
mrsergio8 <34063723+mrsergio8@users.noreply.github.com>
2025-07-31 20:35:55 +0200

    Revert "Develop sergio (#2)"
    This reverts commit 68f197093be4ecd66caa9d43a35529fc667fa6ad.

--- AuthService/AuthService/Dtos/LoginRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AuthService.Dtos
-{
-    public class LoginRequest
-    {
-        public string Email { get; set; } = null!;
-
-        public string Password { get; set; } = null!;
-    }
-}
--- AuthService/AuthService/Program.cs
+++ AuthService/AuthService/Program.cs
@@ -1,8 +1,6 @@
 using AuthService.Data;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System.Security.Claims;
-using System.Text;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,26 +17,25 @@ builder.Services.AddDbContext<AuthDbContext>(options =>
     options.UseNpgsql(connectionString));
 
 // Add services to the container.
+
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
-app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
-
-
-
