9d6abaf2bd7fc1d0205e82f2a694aaf5defeb915
mrsergio8 <34063723+mrsergio8@users.noreply.github.com>
2025-07-31 20:44:23 +0200

    Develop sergio (#4)
    * DDBB Migrations
    
    * Endpoint Register + dato resgister request

--- AuthService/AuthService/Controllers/UserController.cs
+++ AuthService/AuthService/Controllers/UserController.cs
@@ -77,5 +77,54 @@ namespace AuthService.Controllers
 
             return Ok(new { token });
         }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+        {
+            //Comprobación de email no vacío
+            if (string.IsNullOrWhiteSpace(request.Email))
+            {
+                return BadRequest(new { message = "El email no puede estar vacío." });
+            }
+
+            //Comprobación email no existente en BD
+            var user = await _context.Users.SingleOrDefaultAsync(u => u.Email == request.Email);
+            if (user != null ) return BadRequest(new { message = "El email ya está registrado." });
+
+            //Comprobación de contraseña no vacía
+            if (string.IsNullOrWhiteSpace(request.Password))
+            {
+                return BadRequest(new { message = "La contraseña no puede estar vacía." });
+            }
+
+            //Comprobación de nombre no vacío
+            if (string.IsNullOrWhiteSpace(request.Name))
+            {
+                return BadRequest(new { message = "El nombre no puede estar vacío." });
+            }
+
+            // Comprobación de edad mínima (16 años)
+            var hoy = DateOnly.FromDateTime(DateTime.UtcNow);
+            var edad = hoy.Year - request.Birthdate.Year - (hoy.DayOfYear < request.Birthdate.DayOfYear ? 1 : 0); //Si aun no ha cumplido años este año se resta 1
+            if (edad < 16)
+            {
+                return BadRequest(new { message = "Debes ser mayor de 16 años para registrarte." });
+            }
+
+            // Crear el nuevo usuario
+            var newUser = new User
+            {
+                Email = request.Email,
+                Password = PasswordHasher.HashPassword(request.Password),//Si la pass se mete hasheada el login no la detecta
+                Name = request.Name,
+                Birthdate = request.Birthdate,
+                CreatedAt = DateTime.UtcNow
+            };
+
+            _context.Users.Add(newUser);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { message = "Usuario registrado correctamente." });
+        }
     }
 }
