17ae77d7f47bf62f3cc3afebd3e8e52c5dee6afb
mrsergio8 <34063723+mrsergio8@users.noreply.github.com>
2025-08-04 20:53:32 +0200

    Merge develop into my dev branch (#8)
    * Mezclados cambios y eliminadas duplicidades (#6)
    
    * Añadido endpoint SignUp. Corregido Login.
    
    * Eliminados elementos duplicados
    
    * Añadida OpositronWebApp con vistas de ejemplo. (#7)
    
    ---------
    
    Co-authored-by: Ulises <73275755+ulises126@users.noreply.github.com>

--- AuthService/AuthService/Controllers/UserController.cs
+++ AuthService/AuthService/Controllers/UserController.cs
@@ -8,6 +8,7 @@ using Microsoft.IdentityModel.JsonWebTokens;
 using System;
 using System.Security.Claims;
 using System.Text;
+using AuthService.Helpers;
 
 namespace AuthService.Controllers
 {
@@ -21,6 +22,8 @@ namespace AuthService.Controllers
         {
             _context = context;
         }
+
+        #region GET
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<User>>> GetUsers()
@@ -48,6 +51,10 @@ namespace AuthService.Controllers
             return user;
         }
 
+        #endregion
+
+        #region POST
+
         [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody] LoginRequest request)
         {
@@ -61,7 +68,8 @@ namespace AuthService.Controllers
                 new Claim(ClaimTypes.Email, user.Email)
             };
 
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("clave-secreta-muy-segura"));
+            // En un futuro tenemos que considerar el uso de un secreto más seguro y almacenarlo de forma segura
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("this_is_a_very_secure_key_with_more_than_32_chars"));
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
             var tokenDescriptor = new SecurityTokenDescriptor
@@ -78,53 +86,56 @@ namespace AuthService.Controllers
             return Ok(new { token });
         }
 
-        [HttpPost("register")]
-        public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+        [HttpPost("signup")]
+        public async Task<IActionResult> SignUp([FromBody] SignUpRequest request)
         {
-            //Comprobación de email no vacío
-            if (string.IsNullOrWhiteSpace(request.Email))
-            {
-                return BadRequest(new { message = "El email no puede estar vacío." });
-            }
+            var userDb = await _context.Users.SingleOrDefaultAsync(u => u.Email == request.Email);
+            if (userDb != null)
+                return Conflict("El usuario ya existe.");
 
-            //Comprobación email no existente en BD
-            var user = await _context.Users.SingleOrDefaultAsync(u => u.Email == request.Email);
-            if (user != null ) return BadRequest(new { message = "El email ya está registrado." });
-
-            //Comprobación de contraseña no vacía
-            if (string.IsNullOrWhiteSpace(request.Password))
-            {
-                return BadRequest(new { message = "La contraseña no puede estar vacía." });
-            }
-
-            //Comprobación de nombre no vacío
             if (string.IsNullOrWhiteSpace(request.Name))
-            {
-                return BadRequest(new { message = "El nombre no puede estar vacío." });
-            }
+                return BadRequest("El nombre es obligatorio.");
 
-            // Comprobación de edad mínima (16 años)
-            var hoy = DateOnly.FromDateTime(DateTime.UtcNow);
-            var edad = hoy.Year - request.Birthdate.Year - (hoy.DayOfYear < request.Birthdate.DayOfYear ? 1 : 0); //Si aun no ha cumplido años este año se resta 1
-            if (edad < 16)
-            {
-                return BadRequest(new { message = "Debes ser mayor de 16 años para registrarte." });
-            }
+            if (request.Password.Length < 8)
+                return BadRequest("La contraseña debe tener al menos 8 caracteres.");
 
-            // Crear el nuevo usuario
-            var newUser = new User
+            if (IsAtLeast16YearsOld(request.Birthdate) == false)
+                return BadRequest("Debes ser mayor de 16 años.");
+
+            User user = new User
             {
                 Email = request.Email,
-                Password = PasswordHasher.HashPassword(request.Password),//Si la pass se mete hasheada el login no la detecta
+                Password = PasswordHasher.HashPassword(request.Password),
                 Name = request.Name,
                 Birthdate = request.Birthdate,
                 CreatedAt = DateTime.UtcNow
             };
 
-            _context.Users.Add(newUser);
-            await _context.SaveChangesAsync();
-
-            return Ok(new { message = "Usuario registrado correctamente." });
+            try
+            {
+                await _context.Users.AddAsync(user);
+                await _context.SaveChangesAsync();
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Error al crear el usuario: " + ex.Message);
+            }
         }
+
+        #endregion
+
+        #region Métodos privados
+
+        private bool IsAtLeast16YearsOld(DateOnly birthDate)
+        {
+            var today = DateOnly.FromDateTime(DateTime.Today);
+            var minimumBirthDate = today.AddYears(-16);
+
+            return birthDate <= minimumBirthDate;
+        }
+
+        #endregion
+
     }
 }
